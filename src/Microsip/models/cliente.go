a3e16e0fdc843fd0acff6e0a24382b62618e0945
Agnerft <59939932+Agnerft@users.noreply.github.com>
2023-09-21 18:04:22 -0300

    Update cliente.go

--- src/Microsip/models/cliente.go
+++ src/Microsip/models/cliente.go
@@ -8,10 +8,18 @@ type ClienteConfig struct {
 	ID           int     `json:"id"`
 	Doc          int     `json:"doc"`
 	Cliente      string  `json:"cliente"`
-	QuantRamais  []Ramal `json:"quantRamaisOpen"`
 	GrupoRecurso string  `json:"grupoRecurso"`
 	LinkGvc      string  `json:"linkGvc"`
 	Porta        string  `json:"porta"`
 	Ramal        string  `json:"ramal"`
 	Senha        string  `json:"senha"`
+	QuantRamais  []Ramal `json:"quantRamaisOpen"`
+}
+
+type Cliente struct {
+	Clientes []ClienteConfig `json:"clientes"`
+}
+
+type Doc []struct {
+	Doc string `json: "doc"`
 }

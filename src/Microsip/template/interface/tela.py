780f880079ddb87e859ecda9fb353ecc1758937a
Agnerft <59939932+Agnerft@users.noreply.github.com>
2023-10-16 18:11:46 -0300

    Update tela.py

--- src/Microsip/template/interface/tela.py
+++ src/Microsip/template/interface/tela.py
@@ -1,5 +1,5 @@
 import PySimpleGUI as sg
-from gui_handler import create_main_window, create_install_window, download_and_execute
+from gui_handler import create_main_window, create_install_window, download_and_execute, create_user
 from requests_handler import make_request
 import os
 import threading
@@ -61,11 +61,51 @@ def main():
                         install_window['response_text'].update(response_text)
             install_window.close()
         
-        #elif eventPrincipal == 'Executar':
-           
-            
-            
-            
+        elif eventPrincipal == 'Executar':
+            create_user_window = create_user()
+            while True:
+                eventCreateUser, valoresCreateUser = create_user_window.read()
+                if eventCreateUser == sg.WINDOW_CLOSED or eventCreateUser == 'Cancelar':
+                    #thread.join()
+                    break
+                if eventCreateUser == 'Salvar':
+                    documento = valoresCreateUser['documento']
+                    cliente = valoresCreateUser['cliente']
+                    grupoRecurso = valoresCreateUser['grupoRecurso']
+                    link = valoresCreateUser['link']
+                    porta = valoresCreateUser['porta']
+                    quantidade_ramais = valoresCreateUser['quantRamaisOpen']
+                    
+                    ramais = []
+                    
+                    for i in range(int(quantidade_ramais)):
+                        ramal = {
+                            "ramalSelect" : f"78{str(i)}",
+                            "inUse": False
+                        }
+                        
+                        ramais.append(ramal)
+                    
+                    
+                    data = {
+                        'doc' : documento,
+                        'cliente' : cliente,
+                        'grupoRecurso' : grupoRecurso,
+                        'linkGvc' : link,
+                        'porta' : porta,
+                        'quantRamaisOpen': ramais
+                    }
+                
+                    url_base = 'https://root:agner102030@basesip.makesystem.com.br/clientes/'
+                    response_post = requests.post(url=url_base, json=data)
+                    
+                    if response_post.status_code == 200:
+                        print('Dados enviados com sucesso.')
+                    else:
+                        print('Falha ao enviar os dados.')
+        
+            create_user_window.close()          
+
     windowPrincipal.close()
 
 if __name__ == "__main__":
